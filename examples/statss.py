96075170cc0fece0ee064e622e7b17c32d1c7594
Dima <diman.suzmin@yandex.ru>
2019-12-13 17:01:32 +0400

    Update 0.8

--- examples/statss.py
+++ examples/statss.py
@@ -1,11 +1,9 @@
 from fnapi import stats as F
 
-
 api = 'HERE PASTE TOKEN FROM https://fortnitetracker.com/site-api'
 platform = 'pc'
 nickname = '1xev3'
 
-F.api_key = api
-player = F.Stats(platform,nickname)
+player = F.Stats(platform,nickname,api_key=api)
 
 print(player.stats.CURRENT_DUO_KD)
